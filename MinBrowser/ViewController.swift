30690a2523f1635e1efa302d5c3658bccea6baa2
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2022-02-16 21:42:01 +0900

    add project & icons

---
+++ MinBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinBrowser
+//
+//  Created by Takuto Nakamura on 2022/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
