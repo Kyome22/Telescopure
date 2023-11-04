d21777bd92025658b05d6fcbb832a7c28d23d7e1
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2023-11-04 21:48:30 +0900

    Delete match

--- Telescopure/Helper/String+Extensions.swift
+++ Telescopure/Helper/String+Extensions.swift
@@ -9,9 +9,4 @@ import Foundation
 
 extension String {
     static let bookmarksJSON = "bookmarksJSON"
-
-    func match(pattern: String) -> Bool {
-        let matchRange = self.range(of: pattern, options: .regularExpression)
-        return matchRange != nil
-    }
 }
