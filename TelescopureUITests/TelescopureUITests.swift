1adfe41d58b56071025598066d6a64de19108722
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-07-20 15:08:58 +0900

    Modified TelescopureUITests

--- TelescopureUITests/TelescopureUITests.swift
+++ TelescopureUITests/TelescopureUITests.swift
@@ -1,62 +1,68 @@
-/*
- TelescopureUITests.swift
- TelescopureUITests
-
- Created by Takuto Nakamura on 2023/10/19.
-*/
-
 import XCTest
 
 final class TelescopureUITests: XCTestCase {
-//    let app = XCUIApplication()
-//
-//    override func setUpWithError() throws {
-//        continueAfterFailure = false
-//        app.launch()
-//    }
-//
-//    func testSearchKeyword() throws {
-//        let searchTextField = app.textFields["searchTextField"]
-//        searchTextField.tap()
-//        searchTextField.typeText("apple\n")
-//
-//        let actual = try XCTUnwrap(searchTextField.value as? String)
-//        let expect = "https://www.google.com/search?q=apple"
-//        XCTAssertEqual(actual, expect)
-//    }
-//
-//    func testClearTextField() throws {
-//        let searchTextField = app.textFields["searchTextField"]
-//        searchTextField.tap()
-//        searchTextField.typeText("abcdefghijklmn")
-//
-//        let clearButton = app.buttons["clearButton"]
-//        clearButton.tap()
-//
-//        let actual = try XCTUnwrap(searchTextField.value as? String)
-//        let expect = "Search…"
-//        XCTAssertEqual(actual, expect)
-//    }
-//
-//    func testHideToolBar() throws {
-//        let hideToolBarButton = app.buttons["hideToolBarButton"]
-//        hideToolBarButton.tap()
-//
-//        let showToolBarButton = app.buttons["showToolBarButton"]
-//        XCTAssertTrue(showToolBarButton.exists)
-//
-//        showToolBarButton.tap()
-//        XCTAssertTrue(hideToolBarButton.exists)
-//    }
-//
-//    func testOpenBookmark() throws {
-//        let showBookmarkButton = app.buttons["showBookmarkButton"]
-//        showBookmarkButton.tap()
-//
-//        let hideBookmarkButton = app.buttons["hideBookmarkButton"]
-//        XCTAssertTrue(hideBookmarkButton.exists)
-//
-//        hideBookmarkButton.tap()
-//        XCTAssertTrue(showBookmarkButton.exists)
-//    }
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testSearchKeyword() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let searchTextField = app.textFields["searchTextField"]
+        searchTextField.tap()
+        searchTextField.typeText("apple\n")
+
+        let actual = try XCTUnwrap(searchTextField.value as? String)
+        let expect = "https://www.google.com/search?q=apple"
+        XCTAssertEqual(actual, expect)
+    }
+
+    @MainActor
+    func testClearTextField() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let searchTextField = app.textFields["searchTextField"]
+        searchTextField.tap()
+        searchTextField.typeText("abcdefghijklmn")
+
+        let clearButton = app.buttons["clearButton"]
+        clearButton.tap()
+
+        let actual = try XCTUnwrap(searchTextField.value as? String)
+        let expect = "Search…"
+        XCTAssertEqual(actual, expect)
+    }
+
+    @MainActor
+    func testHideToolBar() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let hideToolBarButton = app.buttons["hideToolBarButton"]
+        hideToolBarButton.tap()
+
+        let showToolBarButton = app.buttons["showToolBarButton"]
+        XCTAssertTrue(showToolBarButton.exists)
+
+        showToolBarButton.tap()
+        XCTAssertTrue(hideToolBarButton.exists)
+    }
+
+    @MainActor
+    func testOpenBookmark() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let showBookmarkButton = app.buttons["showBookmarkButton"]
+        showBookmarkButton.tap()
+
+        let hideBookmarkButton = app.buttons["hideBookmarkButton"]
+        XCTAssertTrue(hideBookmarkButton.exists)
+
+        hideBookmarkButton.tap()
+        XCTAssertTrue(showBookmarkButton.exists)
+    }
 }
