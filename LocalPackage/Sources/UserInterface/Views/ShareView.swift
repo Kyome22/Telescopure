ae769cd1434f1cce0cc68344e79066f1c701cb27
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-07-20 13:03:29 +0900

    Fixed ShareView Layout

--- LocalPackage/Sources/UserInterface/Views/ShareView.swift
+++ LocalPackage/Sources/UserInterface/Views/ShareView.swift
@@ -10,7 +10,7 @@ public struct ShareView: View {
     }
 
     public var body: some View {
-        VStack(spacing: 0) {
+        VStack(spacing: 16) {
             Button {
                 Task {
                     await store.send(.cancelButtonTapped)
@@ -18,15 +18,15 @@ public struct ShareView: View {
             } label: {
                 Text("cancel", bundle: .module)
             }
-            .padding(16)
             Color(.systemGray3)
-                .frame(maxWidth: .infinity, minHeight: 1, maxHeight: 1)
+                .frame(height: 1)
+                .frame(maxWidth: .infinity)
             Text(store.sharedType.sharedText)
                 .lineLimit(7)
                 .foregroundColor(Color.primary)
-                .padding(16)
             Color(.systemGray3)
-                .frame(maxWidth: .infinity, minHeight: 1, maxHeight: 1)
+                .frame(height: 1)
+                .frame(maxWidth: .infinity)
             Button {
                 Task {
                     await store.send(.openButtonTapped)
@@ -34,9 +34,10 @@ public struct ShareView: View {
             } label: {
                 Text(store.sharedType.label)
             }
-            .padding(16)
         }
+        .padding(16)
         .background(Color(.systemGray5), in: .rect(cornerRadius: 12))
+        .compositingGroup()
         .shadow(radius: 8)
         .padding(40)
         .task {
