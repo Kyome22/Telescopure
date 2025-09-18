9fabad6b7ca2dd9f06124be31a083c4fa5cf8f0c
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-09-19 04:30:58 +0900

    Applied Glass Effect to ShowToolBarButton

--- LocalPackage/Sources/UserInterface/Views/ShowToolBarButton.swift
+++ LocalPackage/Sources/UserInterface/Views/ShowToolBarButton.swift
@@ -2,27 +2,51 @@ import Model
 import SwiftUI
 
 struct ShowToolBarButton: View {
+    @ScaledMetric private var imageSize = 40
     var store: Browser
 
     var body: some View {
+        if #available(iOS 26, *) {
+            button
+                .glassEffect()
+        } else {
+            button
+                .shadow(radius: 8)
+        }
+    }
+
+    var button: some View {
         Button {
             Task {
                 await store.send(.showToolBarButtonTapped)
             }
         } label: {
-            Label {
-                Text("showToolBar", bundle: .module)
-            } icon: {
-                Image(systemName: "chevron.up")
-                    .imageScale(.large)
-                    .frame(width: 40, height: 40)
+            if #available(iOS 26, *) {
+                Label {
+                    Text("showToolBar", bundle: .module)
+                } icon: {
+                    Image(systemName: "chevron.up")
+                        .imageScale(.large)
+                        .frame(width: imageSize, height: imageSize)
+                }
+                .labelStyle(.iconOnly)
+                .padding(8)
+            } else {
+                Label {
+                    Text("showToolBar", bundle: .module)
+                } icon: {
+                    Image(systemName: "chevron.up")
+                        .imageScale(.large)
+                        .frame(width: imageSize, height: imageSize)
+                }
+                .labelStyle(.iconOnly)
+                .padding(8)
+                .background(Color(.floating))
             }
-            .labelStyle(.iconOnly)
         }
         .buttonStyle(.borderless)
-        .background(Color(.floating), in: Circle())
+        .buttonBorderShape(.circle)
         .accessibilityIdentifier("showToolBarButton")
-        .shadow(radius: 8)
     }
 }
 
