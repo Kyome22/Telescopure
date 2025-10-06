4e536ea50316ec1bc9787d637b3d75293539bd2c
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-10-07 02:29:24 +0900

    Removed throws

--- TelescopureUITests/TelescopureUITests.swift
+++ TelescopureUITests/TelescopureUITests.swift
@@ -37,7 +37,7 @@ final class TelescopureUITests: XCTestCase {
     }
 
     @MainActor
-    func testHideToolbar() throws {
+    func testHideToolbar() {
         let app = XCUIApplication()
         app.launch()
 
@@ -52,7 +52,7 @@ final class TelescopureUITests: XCTestCase {
     }
 
     @MainActor
-    func testOpenBookmark() throws {
+    func testOpenBookmark() {
         let app = XCUIApplication()
         app.launch()
 
